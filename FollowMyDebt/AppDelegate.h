e34bf3b6693e6acfd7266a1e3fb9a7ed032a0816
Loris Venturelli <venturelli.loris@gmail.com>
2015-03-18 14:52:31 +0100

    Initial Commit

---
+++ FollowMyDebt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FollowMyDebt
+//
+//  Created by Loris on 18/03/2015.
+//  Copyright (c) 2015 Loris Venturelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FollowMyDebt/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FollowMyDebt
+//
+//  Created by Loris on 18/03/2015.
+//  Copyright (c) 2015 Loris Venturelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
